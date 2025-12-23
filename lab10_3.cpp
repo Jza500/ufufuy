fb4ae0900966e0e97240845e4252053562144838
Jza500 <kittiphob_i@cmu.ac.th>
2025-12-23 16:49:26 +0700

    sedkub 3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include <cmath>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    int count = 0;
+    float sum = 0;
+    float sum_of_square = 0;
+
+    string Leg;
+    while (getline(source,Leg))
+    {
+        sum += stof(Leg);
+        sum_of_square += pow(stof(Leg),2);
+
+        count++;
+
+    }    
+    float Mean = sum/count;
+    float SD = sqrt(sum_of_square/count-pow(Mean,2));
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << Mean << endl;
+    cout << "Standard deviation = " << SD;
+}
